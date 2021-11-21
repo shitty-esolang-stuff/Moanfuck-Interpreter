815639e4d595c2f8aac809e493f2a1650f0ceeb1
Treyver Reicha <70043252+emerald73@users.noreply.github.com>
2021-11-21 18:02:47 -0500

    clean up code

--- swap.py
+++ swap.py
@@ -1,4 +1,6 @@
-str = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+# This file is for translating brainfuck code into the proper keywords to be used by the interpreter.
+
+str = ">>INSERT BRAINFUCK CODE HERE<<"
 
 swap = str.replace("+", "AH").replace("-", "OH").replace(">", "YES").replace("<", "FUCK").replace(",", "MORE").replace(".", "YEAH").replace("[", "AHH").replace("]", "OOH")
 
