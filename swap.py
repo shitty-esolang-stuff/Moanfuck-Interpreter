8be550d2608702d0ffac99bf1d449d70b94baff0
Treyver Reicha <70043252+emerald73@users.noreply.github.com>
2021-11-20 21:00:51 -0500

    add swap.py

---
+++ swap.py
@@ -0,0 +1,5 @@
+str = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+
+swap = str.replace("+", "AH").replace("-", "OH").replace(">", "YES").replace("<", "FUCK").replace(",", "MORE").replace(".", "YEAH").replace("[", "AHH").replace("]", "OOH")
+
+print(swap)
